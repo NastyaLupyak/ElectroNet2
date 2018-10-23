259ee946bd8f5f148c79c3c0545cf7175dd34dd0
NastyaLupyak <44192883+NastyaLupyak@users.noreply.github.com>
2018-10-23 20:33:07 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,19 +1,20 @@
 
 public class Calculator {
 	
+	// функція для отримання значення миттєвої потужності користувача 
 	public static double calculatePowerOnUser(User obj)
 	{
-		double P;
-		if(obj.IsUserON() == true && obj.IsUserConnected() == true)
+		double P; // потужність користувача
+		if(obj.IsUserON() == true && obj.IsUserConnected() == true) // якщо користувач вімкнено і під'єданно до генератора
 		{
-		double I = obj.GetUserI();
-		double R = obj.GetUserR();
-		P=I*I*R;
+		double I = obj.GetUserI(); // отримати значення сили струму з об'єкта класа User
+		double R = obj.GetUserR(); // отримати значення опору з об'єкта класа User
+		P=I*I*R; // формула для знаходження потужності користувача 
 		}
 		else 
 			P=0;
 		
-		return P;
+		return P; // повертаємо значення потужності 
 	}
 	
 }
