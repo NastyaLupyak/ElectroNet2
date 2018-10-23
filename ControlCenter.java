fc40dfe963c15aa763482e916d4d0f468c80ea06
NastyaLupyak <44192883+NastyaLupyak@users.noreply.github.com>
2018-10-23 20:15:41 +0300

    Update ControlCenter.java

--- ControlCenter.java
+++ ControlCenter.java
@@ -1,30 +1,32 @@
 
 public class ControlCenter {
 	
+	// в функції main створюємо генератори та користувачів, зв'язуємо їх
 	public static void main(String[] args)
 	{
 		Generator Gen_obj1 = new Generator(100, 1, true);
 		User obj1 = new User(5, true);
+		Calculator calc_obj = new Calculator();
 		System.out.println(obj1.GetUserI());
 		initializeConnection(Gen_obj1, obj1);
 		System.out.println(obj1.GetUserI());
+		System.out.println(calc_obj.calculatePowerOnUser(obj1));
 	}
-	
+	// функція для ініціалізації звязку між генератором та користувачем
 	public static void initializeConnection(Generator objGenerator, User objUser)
 	{
-		if(objGenerator.isAvialableForConnection() == true)
+		if(objGenerator.isAvialableForConnection() == true) // якщо генератор доступний для з'єднання 
 		{
-			objGenerator.NumOfCurrentUsers++;
-			objUser.Connected = true;
-			if(objUser.IsUserON() == true)
+			objGenerator.NumOfCurrentUsers++; // інкрементуємо змінну, що позначає кількість під'єднанних до генератора користувачив
+			objUser.Connected = true; // задаємо значення true до змінної, що позначає чи з'єднан користувач з генератором
+			if(objUser.IsUserON() == true) // якщо користувач вімнкнено
 			{
-				objUser.Connected = true;
-				objUser.I = objGenerator.GetGenI();
+				objUser.I = objGenerator.GetGenI(); // сила струмум на користувачі = силі струмму, що подає генератор
 			}
-			else 
-				objUser.I = 0;
+			else // користувач вимкнений 
+				objUser.I = 0; // струм на ньому не тече
 		}
-		else 
+		else // генератор не доступний для з'єднання з користувачем 
 			System.out.println("Too many users on currrent generator. Impossible for connection.");
 	}
 	
