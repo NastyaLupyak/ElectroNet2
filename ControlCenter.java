b1e7e3ee3bf4e0385f877cb8ab908a8ca9208109
NastyaLupyak <44192883+NastyaLupyak@users.noreply.github.com>
2018-12-12 21:49:29 +0200

    Update ControlCenter.java

--- ControlCenter.java
+++ ControlCenter.java
@@ -1,32 +1,47 @@
-
+import java.util.*; 
 public class ControlCenter {
 	
-	// в функції main створюємо генератори та користувачів, зв'язуємо їх
 	public static void main(String[] args)
 	{
-		Generator Gen_obj1 = new Generator(100, 1, true);
+		Generator Gen_obj1 = new Generator(100, 2, true);
 		User obj1 = new User(5, true);
 		Calculator calc_obj = new Calculator();
-		System.out.println(obj1.GetUserI());
+		Search search_obj = new Search();
 		initializeConnection(Gen_obj1, obj1);
-		System.out.println(obj1.GetUserI());
-		System.out.println(calc_obj.calculatePowerOnUser(obj1));
+		
+		ArrayList<User> listOfTVs = new ArrayList<>();
+		listOfTVs.add(new TV(35, true, "Samsung"));
+		listOfTVs.add(new TV(15, true, "Philips"));
+		Sort.SortByR(listOfTVs);
+		search_obj.SearchR(listOfTVs, 35);
+		search_obj.SearchR(listOfTVs, 20);
+		search_obj.SearchName(listOfTVs, "Samsung");
+		
+		System.out.println(listOfTVs.get(0).I);
+		initializeConnection(Gen_obj1, listOfTVs.get(0));
+		System.out.println(listOfTVs.get(0).I);
+		
+		Sort.SortByPower(listOfTVs, calc_obj);
+		search_obj.SearchPower(listOfTVs, calc_obj, 150);
+		search_obj.SearchPower(listOfTVs, calc_obj, 150000);
 	}
-	// функція для ініціалізації звязку між генератором та користувачем
+	
 	public static void initializeConnection(Generator objGenerator, User objUser)
 	{
-		if(objGenerator.isAvialableForConnection() == true) // якщо генератор доступний для з'єднання 
+		if(objGenerator.isAvialableForConnection() == true)
 		{
-			objGenerator.NumOfCurrentUsers++; // інкрементуємо змінну, що позначає кількість під'єднанних до генератора користувачив
-			objUser.Connected = true; // задаємо значення true до змінної, що позначає чи з'єднан користувач з генератором
-			if(objUser.IsUserON() == true) // якщо користувач вімнкнено
+			objGenerator.NumOfCurrentUsers++;
+			objUser.Connected = true;
+			if(objUser.IsUserON() == true)
 			{
-				objUser.I = objGenerator.GetGenI(); // сила струмум на користувачі = силі струмму, що подає генератор
+				objUser.Connected = true;
+				objUser.I = objGenerator.GetGenI();
+				System.out.println("Connection initialized.");
 			}
-			else // користувач вимкнений 
-				objUser.I = 0; // струм на ньому не тече
+			else 
+				objUser.I = 0;
 		}
-		else // генератор не доступний для з'єднання з користувачем 
+		else 
 			System.out.println("Too many users on currrent generator. Impossible for connection.");
 	}
 	
